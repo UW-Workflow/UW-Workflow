f69047628b935618cb7b9b79a3f8f3022df0773e
Tony Kappen <tkappen.neppak@gmail.com>
2021-11-11 18:32:39 -0500

    got it centered

--- components/MainContainer.tsx
+++ components/MainContainer.tsx
@@ -4,7 +4,7 @@ import { Header } from "./Header";
 export const MainContainer: React.FC = ({ children }) => (
   <div className="container mx-auto max-w-full flex flex-col min-h-screen">
     <Header/>
-      <div className="flex-grow">
+      <div className="flex-grow items-center">
         {children}
       </div>
     <Footer/>
--- pages/404.tsx
+++ pages/404.tsx
@@ -4,15 +4,15 @@ import React from "react";
 export default function PageNotFound404() {
   return (      
     <MainContainer>
-    <div className="flex items-center fit-content">
-      <div className="flex items-center flex-col">
-          <img src="illustration.png" />
-          <p className="font-extrabold text-5xl">Oops!</p>
-          <p>You weren't supposed to see this page...yeti here you are</p>
-          <div className="bg-button-blue text-white rounded-xl p-3 flex items-center space-x-2">
-            <span>Go back to the home page</span>
-          </div>
-      </div>
+    <div className="flex flex-grow">
+        <div className="flex flex-grow flex-col items-center">
+            <img src="illustration.png" />
+            <p className="font-extrabold text-5xl">Oops!</p>
+            <p>You weren't supposed to see this page...yeti here you are</p>
+            <div className="bg-button-blue text-white rounded-xl p-3 flex items-center space-x-2">
+                <span>Go back to the home page</span>
+            </div>
+        </div>
     </div>
     </MainContainer>
   );
