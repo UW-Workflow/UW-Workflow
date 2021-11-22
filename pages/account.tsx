7708b68b1ac727263a828c130d916ef8afbe98cd
Tony Kappen <tkappen.neppak@gmail.com>
2021-11-21 22:07:24 -0500

    need to get colors to match

--- pages/account.tsx
+++ pages/account.tsx
@@ -9,25 +9,25 @@ export default function YourAccount(){
                 </div>
                 <div className="flex">
                     <ul className="flex">
-                        <li className="p-4 border-b-2 border-transparent hover:border-blue-active">
-                            <a className="font-medium text-black-500 hover:text-gray-500" href="#">Profile</a>
+                        <li className="p-4 border-b-2 border-transparent hover:border-blue-active hover:text-gray-500">
+                            <a className="font-medium text-black-500 " href="#">Profile</a>
                         </li>
-                        <li className="p-4 border-b-2 border-transparent hover:border-blue-active">
-                            <a className="font-medium text-black-500 hover:text-gray-500" href="#">Bookmarks</a>
+                        <li className="p-4 border-b-2 border-transparent hover:border-blue-active hover:text-gray-500">
+                            <a className="font-medium text-black-500 " href="#">Bookmarks</a>
                         </li>
-                        <li className="p-4 border-b-2 border-transparent hover:border-blue-active">
-                            <a className="font-medium text-black-500 hover:text-gray-500" href="#">Password</a>
+                        <li className="p-4 border-b-2 border-transparent hover:border-blue-active hover:text-gray-500">
+                            <a className="font-medium text-black-500" href="#">Password</a>
                         </li>
                     </ul>
                     <div className="bg-logout-bg text-logout-text font-semibold rounded-xl py-2 px-8 flex items-center space-x-2 ml-auto mr-20 mb-4">
                         <img src="Logout.svg"></img>
                         <span>Log out</span>
                     </div>
-
                 </div>
                 <hr className="mr-20"/>
+                <div className="self-center bg-gradient-2 filter rounded-full blur-huge h-gradient-2-circle w-gradient-2-circle -ml-auto"></div>
+                <div className="self-center bg-gradient-2 filter rounded-full blur-huge h-gradient-2-circle w-gradient-2-circle ml-auto"></div>
             </div>
-            
         </MainContainer>
     )
 }
