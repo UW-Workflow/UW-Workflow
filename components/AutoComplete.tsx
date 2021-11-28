8971687dfc777d42604b37e19192db5f6f6be9de
Sneh Koul <snehkoul1999@gmail.com>
2021-11-28 00:34:49 -0500

    searching for a company working

--- components/AutoComplete.tsx
+++ components/AutoComplete.tsx
@@ -1,60 +1,29 @@
-import React from 'react'
-import { ReactSearchAutocomplete } from 'react-search-autocomplete'
-
-function AutoComplete(props) {
-  const items = props.items
-  const handleOnSearch = (string, results) => {
-    // onSearch will have as the first callback parameter
-    // the string searched and for the second the results.
-    if (props.handleOnSearch){
-      return props.handleOnSearch(string,results)
-    }
-  }
-
-  const handleOnHover = (result) => {
-    // the item hovered
-    if (props.handleOnHover){
-      return props.handleOnHover(result)
-    }
-  }
-
-  const handleOnSelect = (item) => {
-    // the item selected
-    if(props.handleOnSelect){
-      return props.handleOnSelect(item)
-    }
-  }
-
-  const handleOnFocus = () => {
-    if(props.handleOnFocus){
-      return props.handleOnFocus()
-    }
-  }
-
-  const formatResult = (item) => {
-    if(props.formatResult){
-      return props.formatResult(item)
-    }
-    return item;
-  }
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div style={{ width: 400 }}>
-          <ReactSearchAutocomplete
-            items={items}
-            onSearch={handleOnSearch}
-            onHover={handleOnHover}
-            onSelect={handleOnSelect}
-            onFocus={handleOnFocus}
-            autoFocus
-            formatResult={formatResult}
-          />
-        </div>
-      </header>
-    </div>
-  )
+import React, { useState } from "react";
+import { ReactSearchAutocomplete } from "react-search-autocomplete";
+import { Company } from "../models/interfaces/types/Company";
+import { useRouter } from "next/router";
+interface AutoCompleteProps {
+  items: Company[];
 }
 
-export default AutoComplete
+export const AutoComplete: React.FC<AutoCompleteProps> = ({ items }) => {
+  const router = useRouter();
+  const handleOnSelect = (item: Company) => {
+    // TODO: Take them to the company page
+    console.log(item);
+    router.push(`/${item.id}`);
+  };
+
+  return (
+    <div style={{ width: 400 }}>
+      <ReactSearchAutocomplete
+        items={items}
+        onSelect={handleOnSelect}
+        autoFocus
+        placeholder={"Search for companies"}
+      />
+    </div>
+  );
+};
+
+export default AutoComplete;
--- pages/home.tsx
+++ pages/home.tsx
@@ -1,7 +1,28 @@
 import { MainContainer } from "../components/MainContainer";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import AutoComplete from "../components/AutoComplete";
+
+import { Company } from "../models/interfaces/types/Company";
+import axios from "axios";
 
 export default function Home() {
+  let [companies, setCompanies] = useState<Company[]>([]);
+  let [searchedCompanies, setSearchedCompanies] = useState<string[]>([]);
+  // fetches the companies on load
+  useEffect(() => {
+    async function getCompanies() {
+      try {
+        const response = await axios.get(`/api/companies`);
+        if (response.data.companies) {
+          setCompanies(response.data.companies);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getCompanies();
+  }, []);
+
   return (
     <>
       <MainContainer>
@@ -21,13 +42,8 @@ export default function Home() {
             </h2>
 
             <div className="flex items-center space-x-4">
-              <input
-                type="text"
-                className="pr-40 pl-10 pt-3 pb-3 rounded-lg drop-shadow-md"
-                placeholder="Search for a company"
-              />
-              {/* Removing this flow */}
-              {/* <span>or</span>
+              <AutoComplete items={companies} />
+              <span>or</span>
               <div className="bg-button-blue text-white rounded-xl p-3 flex items-center space-x-2">
                 <div className="bg-white text-button-blue rounded-md">
                   <svg
@@ -45,7 +61,7 @@ export default function Home() {
                   </svg>
                 </div>
                 <span>Add a company</span>
-              </div> */}
+              </div>
             </div>
           </div>
           <div className="flex flex-col space-y-4">
