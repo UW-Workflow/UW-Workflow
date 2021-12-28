46322a3e17a2c144d2f780339c52fa5099fd1435
Sneh Koul <snehkoul1999@gmail.com>
2021-12-28 17:58:17 -0500

    add a company completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,13 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    height: {
-      "gradient-2-circle": "408px",
-    },
-    width: {
-      "gradient-2-circle": "408px",
-    },
-    // fontSize:{
-    //   xl:['48px','72px']
-    // },
     extend: {
+      height: {
+        "gradient-2-circle": "408px",
+      },
+      width: {
+        "gradient-2-circle": "408px",
+      },
       blur: {
         small: "60px",
         huge: "128px",
