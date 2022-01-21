50190ff1df07e7016fe06b3ea8c67751e053c776
Akshaya Rajagopalan <akshaya@canix.com>
2022-01-21 13:50:00 -0500

    mobile design for contact us

--- pages/contactus.tsx
+++ pages/contactus.tsx
@@ -31,55 +31,56 @@ export default function ConatctUs() {
       event.preventDefault();
       const finalSubject =
         subject !== "" ? subject : `New message from ${firstName} ${lastName}`;
-      init(EMAILJS_USER_ID);
-      send(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, {
-        from_name: `${firstName} ${lastName}`,
-        subject: finalSubject,
-        message: message,
-        reply_to: authUser ? authUser.email : email,
-      });
+      // init(EMAILJS_USER_ID);
+      // send(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, {
+      //   from_name: `${firstName} ${lastName}`,
+      //   subject: finalSubject,
+      //   message: message,
+      //   reply_to: authUser ? authUser.email : email,
+      // });
       setAfterSuccess(true);
     }
   };
 
   return (
     <MainContainer>
-      <div className="flex flex-col z-10 mx-16 mt-7 items-center mb-2">
+      <div className="flex flex-col z-10 lg:mx-16 md:mx-12 sm:mx-5 mt-7 items-center mb-2">
         <div
           style={{
-            width: "85%",
+            width: "90%",
             borderBottomLeftRadius: 100,
             borderBottomRightRadius: 100,
           }}
           className="flex bg-contact-us-gradient bg-opacity-60"
         >
           <div style={{ width: "100%" }} className="flex mt-3 mb-20">
-            <img width="150px" height="auto" src="/contact-us-left.svg" />
+            <img width="15%" height="auto" src="/contact-us-left.svg" />
             <div
               style={{ width: "100%" }}
-              className="flex flex-col mt-6 ml-32 mr-32 items-center"
+              className="flex flex-col mt-6 md:mt-4 sm:mt-4 lg:ml-30 lg:mr-30 md:ml-12 md:mr-12 sm:ml-8 sm:mr-8 items-center"
             >
-              <h1 className="mb-3 text-5xl font-cabinet-grotesk font-extrabold">
+              <h1 className="lg:mb-3 md:mb-2 sm:mb-1.5 mb-1.5 lg:text-5xl md:text-4xl sm:text-3xl text-2xl font-cabinet-grotesk md:font-bold sm:font-bold lg:font-bold font-medium">
                 Contact Us
               </h1>
-              <h4 className="font-cabinet-grotesk text-gray-500">
-                Have a question or just want to say hi?
+              <h4 className="font-cabinet-grotesk text-sm lg:text-sm md:text-sm sm:text-sm text-gray-500">
+                Have a question or want to say hi?
               </h4>
-              <h4 className="font-cabinet-grotesk -mt-1 text-gray-500">
+              <h4 className="font-cabinet-grotesk mt-1 xl:mt-2 lg:-mt-1 md:md-1 sm:mt-0.5 text-gray-500 text-sm lg:text-sm md:text-sm sm:text-sm">
                 We’d love to hear from you.
               </h4>
             </div>
-            <img width="150px" height="auto" src="/contact-us-right.svg" />
+            <img width="15%" height="auto" src="/contact-us-right.svg" />
           </div>
         </div>
-        <div className="flex items-center -mt-20 bg-white rounded-3xl shadow-lg">
+        <div className="flex items-center w-80 xl:w-auto lg:w-auto md:w-auto sm:w-auto lg:-mt-20 md:-mt-16 sm:-mt-16 xl:-mt-24 -mt-14 bg-white rounded-3xl shadow-lg">
           <img
             width={!authUser && !afterSuceess ? "45%" : "40%"}
             height="auto"
+            className="hidden sm:block md:block lg:block xl:block 2xl:block"
             src={
               afterSuceess
-                ? "/contact-success-image.svg"
-                : "/contact-form-image.svg"
+                ? "/contact-success-image.png"
+                : "/contact-form-image.png"
             }
           />
           {afterSuceess ? (
@@ -87,11 +88,13 @@ export default function ConatctUs() {
               style={{ width: "280px" }}
               className={
                 "flex flex-col text-center items-center " +
-                (!authUser && !afterSuceess ? "ml-12 -mr-12" : "ml-14 -mr-14")
+                (!authUser && !afterSuceess
+                  ? "xl:ml-12 xl:-mr-12 lg:ml-12 lg:-mr-12 md:ml-12 md:-mr-12 sm:ml-12 sm:-mr-12 ml-8 -mr-8"
+                  : "lg:ml-14 lg:-mr-14 md:ml-14 md:-mr-14 sm:ml-14 sm:-mr-14 xl:ml-14 xl:-mr-14 ml-6 -mr-6 xl:h-auto lg:h-auto md:h-auto sm:h-auto h-96")
               }
             >
-              <img width="150px" className="mb-2" src="/email-sent-image.svg" />
-              <p className="font-extrabold text-base mb-1">
+              <img width="150px" className="mb-2" src="/email-sent-image.png" />
+              <p className="lg:font-extrabold md:font-extrabold sm:font-extrabold xl:font-extrabold text-base mb-1">
                 Thanks,&nbsp;{firstName}
               </p>
               <p className="text-sm text-gray-500 mb-3">
@@ -109,20 +112,24 @@ export default function ConatctUs() {
           ) : (
             <div style={{ width: "80%" }} className="flex flex-col m-5">
               <form onSubmit={handleSubmit}>
-                <p className="ml-10 font-cabinet-grotesk">First Name</p>
+                <p className="xl:ml-10 lg:ml-10 md:ml-10 sm:ml-10 ml-5 font-cabinet-grotesk">
+                  First Name
+                </p>
                 <input
                   value={firstName}
                   onChange={(e) => setFirstName(e.target.value)}
                   name="firstName"
                   type="input"
                   style={{ width: "90%" }}
-                  className="p-2 rounded-lg drop-shadow-md border-2 mx-10 my-2 font-cabinet-grotesk"
+                  className="p-2 rounded-lg drop-shadow-md border-2 lg:mx-10 xl:mx-10 md:mx-10 sm:mx-10 mx-4 my-2 font-cabinet-grotesk"
                   placeholder="Please enter your first name"
                   required
                 />
 
                 <div className="flex justify-between">
-                  <p className="ml-10 font-cabinet-grotesk">Last Name</p>
+                  <p className="font-cabinet-grotesk xl:ml-10 lg:ml-10 md:ml-10 sm:ml-10 ml-5">
+                    Last Name
+                  </p>
                   <p className="text-gray-500 font-cabinet-grotesk">Optional</p>
                 </div>
                 <input
@@ -131,20 +138,22 @@ export default function ConatctUs() {
                   name="lastName"
                   type="input"
                   style={{ width: "90%" }}
-                  className="p-2 rounded-lg drop-shadow-md border-2 mx-10 my-2 font-cabinet-grotesk"
+                  className="p-2 rounded-lg drop-shadow-md border-2 my-2 font-cabinet-grotesk lg:mx-10 xl:mx-10 md:mx-10 sm:mx-10 mx-4"
                   placeholder="Please enter your first name"
                 />
 
                 {!authUser && (
                   <div>
-                    <p className="ml-10 font-cabinet-grotesk">Email</p>
+                    <p className="font-cabinet-grotesk xl:ml-10 lg:ml-10 md:ml-10 sm:ml-10 ml-5">
+                      Email
+                    </p>
                     <input
                       value={email}
                       onChange={(e) => setEmail(e.target.value)}
                       name="email"
                       type="input"
                       style={{ width: "90%" }}
-                      className="p-2 rounded-lg drop-shadow-md border-2 mx-10 my-2 font-cabinet-grotesk"
+                      className="p-2 rounded-lg drop-shadow-md border-2 my-2 font-cabinet-grotesk lg:mx-10 xl:mx-10 md:mx-10 sm:mx-10 mx-4"
                       placeholder="Please enter your email"
                       required
                     />
@@ -152,7 +161,9 @@ export default function ConatctUs() {
                 )}
 
                 <div className="flex justify-between">
-                  <p className="ml-10 font-cabinet-grotesk">Subject</p>
+                  <p className="font-cabinet-grotesk xl:ml-10 lg:ml-10 md:ml-10 sm:ml-10 ml-5">
+                    Subject
+                  </p>
                   <p className="text-gray-500 font-cabinet-grotesk">Optional</p>
                 </div>
                 <input
@@ -161,18 +172,20 @@ export default function ConatctUs() {
                   name="subject"
                   style={{ width: "90%" }}
                   placeholder="What is the message regarding?"
-                  className="p-2 rounded-lg drop-shadow-md border-2 mx-10 my-2 font-cabinet-grotesk"
+                  className="p-2 rounded-lg drop-shadow-md border-2 my-2 font-cabinet-grotesk lg:mx-10 xl:mx-10 md:mx-10 sm:mx-10 mx-4"
                   type="input"
                 />
 
-                <p className="ml-10">Message</p>
+                <p className="xl:ml-10 lg:ml-10 md:ml-10 sm:ml-10 ml-5">
+                  Message
+                </p>
                 <textarea
                   value={message}
                   onChange={(e) => setMessage(e.target.value)}
                   name="message"
                   style={{ width: "90%", borderColor: "rgba(229, 231, 235)" }}
                   placeholder="What's on your mind?"
-                  className="p-2 rounded-lg drop-shadow-md border-2 text-sm mx-10 my-2 font-cabinet-grotesk"
+                  className="p-2 rounded-lg drop-shadow-md border-2 text-sm my-2 font-cabinet-grotesk lg:mx-10 xl:mx-10 md:mx-10 sm:mx-10 mx-4"
                   required
                 />
 
@@ -181,7 +194,7 @@ export default function ConatctUs() {
                 <input
                   type="submit"
                   value="Submit"
-                  className="p-2 ml-10 mt-1 bg-blue-600 text-white rounded-md cursor-pointer"
+                  className="p-2 ml-5 xl:ml-10 lg:ml-10 md:ml-10 sm:ml-10 mt-1 bg-blue-600 text-white rounded-md cursor-pointer"
                   style={{ width: "90%" }}
                 />
                 {error && <p style={{ color: "red" }}>{error}</p>}
