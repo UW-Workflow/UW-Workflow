60f7487d7cf2f5dcc371f98ba5c6bb02ab759538
Tony Kappen <tkappen.neppak@gmail.com>
2022-01-24 13:13:59 -0500

    modify ui to remove city country description

--- components/Bookmarks.tsx
+++ components/Bookmarks.tsx
@@ -116,7 +116,7 @@ export default function Bookmarks() {
             })}
         </div>
       )}
-      {bookmarks.length == 0 && <div className="text-gray-500 my-auto">No bookmarks selected</div>}
+      {bookmarks.length == 0 && <div className="text-gray-500 my-auto mx-auto">No bookmarks selected</div>}
       <div className="self-center bg-gradient-3 filter blur-huge px-20 py-10 mt-5 flex-grow hidden sm:block"></div>
       <div>
         <ToastContainer
