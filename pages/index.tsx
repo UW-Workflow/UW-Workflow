4a8e2be1aa05e7fdb193d94b6784db6e4cb7c867
hhaardik <hhaardik@mc-3003-07.student.cs.uwaterloo.ca>
2022-02-08 18:58:47 -0500

    Filtering working

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,10 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import React from "react";
-import { Footer } from "../components/Footer";
-import { Header } from "../components/Header";
 import Home from "./home";
 
 const Index: NextPage = () => {
   return (
     <>
-      <Head>
-        <title> UW Workflow </title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Home />
     </>
   );
